1d99162541f40c022c4bcff07274d31815386262
AaronDeluna <aaron_deluna@mail.ru>
2024-09-25 21:49:30 +0300

    fix: Часть небоскребы поменял на Set

--- src/homework4/ex2/Runner.java
+++ src/homework4/ex2/Runner.java
@@ -5,7 +5,7 @@ import java.util.Set;
 
 public class Runner {
     public static void main(String[] args) {
-        Set<String> words = new HashSet<>(Set.of("тон", "тополь", "боль", "рой", "стройка"));
+        Set<String> words = Set.of("тон", "тополь", "боль", "рой", "стройка");
 
         long count = words.stream()
                 .mapToLong(word -> word.chars()
